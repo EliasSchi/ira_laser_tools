6bf6a5d8ad70cf48a21861b9e4b75615288ef709
Stephen Adhisaputra <stephen.adhisaputra@enabl-tech.de>
2023-12-19 10:22:01 +0000

    ROS2 Parameter loading

--- src/ira_laser_tools/launch/laserscan_multi_merger_launch.py
+++ src/ira_laser_tools/launch/laserscan_multi_merger_launch.py
@@ -8,31 +8,23 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    # Specify name of param file that should be loaded
-    laserscan_multimerger_cmd = Node(package='ira_laser_tools',
-                                     executable='laserscan_multi_merger',
-                                     name='laserscan_multi_merger_node',
-                                     parameters=[
-                                         {'destination_frame': "odom"},
-                                         {'scan_destination_topic': "/merged_scan"},
-                                         {'laserscan_topics': "/lidar_front/scan "
-                                                              "/lidar_left/scan "
-                                                              "/lidar_right/scan "
-                                          },
-                                         {'angle_min': -1.57079632679},
-                                         {'angle_max': 1.57079632679},
-                                         {'angle_increment': 0.01163552834},
-                                         {'scan_time': 0.1},
-                                         {'range_min': 0.1},
-                                         {'range_max': 10.0},
-                                         {'use_inf': False},
-                                         {'allow_scan_delay': True},
-                                         {'max_delay_scan_time': 0.05},
-                                         {'max_completion_time': 0.1},
-                                         {'max_merge_time_diff': 0.1},
-                                         {'best_effort': True},
-                                     ],
-                                     )
+    # Get the directory of your package
+    package_dir = get_package_share_directory('ira_laser_tools')
+
+    # Path to the YAML file
+    yaml_file = os.path.join(package_dir, 'config', 'laserscan_multi_merger.yaml')
+
+    # Load the YAML file
+    with open(yaml_file, 'r') as file:
+        params = yaml.safe_load(file)['laserscan_multi_merger_node']['ros__parameters']
+
+    # Create the Node with parameters loaded from the YAML file
+    laserscan_multimerger_cmd = Node(
+        package='ira_laser_tools',
+        executable='laserscan_multi_merger',
+        name='laserscan_multi_merger_node',
+        parameters=[params],
+    )
 
     return LaunchDescription([
         laserscan_multimerger_cmd,
